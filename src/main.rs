5576b44b1a1f92dd37f767c1310b30ac21a48451
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-20 20:37:56 +0000

    Read http req

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::{
+    io::{BufRead, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:4242").unwrap();
+    println!("Listening on 127.0.0.1:4242");
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("New connection: {}", stream.peer_addr().unwrap());
+        handle_stream(stream);
+    }
+}
+
+fn handle_stream(stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let lines: Vec<_> = buf_reader
+        .lines()
+        .map(|res| res.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    println!("Req: {lines:#?}");
 }
